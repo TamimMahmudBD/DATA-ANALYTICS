d564918888b71b81c22955e30e1459ea364a3939
tamim-jr <internationalinf24@gmail.com>
2023-08-13 22:51:32 +0600

    change data

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
       <div onClick={handleNav} className='block  md:hidden'>
         {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
         <div className={nav ? ' fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-          <h1 className='w-full text-3xl font-bold text-[#219ebc] m-4 ' >REACT.</h1>
+          <h1 className='w-full text-3xl font-bold text-[#219ebc] m-4 ' >DATA STORE</h1>
 
           <ul className='p-4 uppercase '>
             <li className='p-4 border-b border-gray-600'>Home</li>
