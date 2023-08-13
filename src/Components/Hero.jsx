198f3d732eaa55da6f7c22b071c4c8355f9daacd
tamim-jr <internationalinf24@gmail.com>
2023-08-13 22:08:54 +0600

    add data

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Typed from 'react-typed';
 const Hero = () => {
   return (
     <div className='text-white'>
@@ -7,8 +6,7 @@ const Hero = () => {
         <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
         <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md: py-6'>Grow with data.</h1>
         <div className='flex justify-center items-center'>
-          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>First, flexible financing for</p>
-          <Typed className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2' strings={['BTB', 'BTC', 'SASS']} typeSpeed={120} backSpeed={140} />
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>First, flexible financing for BTB, BTC, SASS.</p>
         </div>
         <p className='md:text-2xl text-xl form-bold text-gray-500 px-5'>Monitor your data analytics to increase revenue for BTB, BTC & SAAS platforms.</p>
         <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
