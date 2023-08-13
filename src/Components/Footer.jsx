2544d249ab6505070f0f84b8cd80479239ba2844
tamim-jr <internationalinf24@gmail.com>
2023-08-13 21:03:06 +0600

    DATA ANALYTICS project by teact js and tailwind css

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import {
+    FaDribbbleSquare,
+    FaFacebookSquare,
+    FaGithubSquare,
+    FaTwitterSquare,
+} from 'react-icons/fa'
+
+const Footer = () => {
+    return (
+        <div className='max-w-[1240px] mx-auto my-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
+            <div>
+                <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+                <p className='py-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error eius quae officiis minus voluptates minima vitae a voluptas sunt fuga.</p>
+                <div className='flex justify-between md:w-[75%] my-6'>
+                    <FaFacebookSquare className='size={30}' />
+                    <FaDribbbleSquare className='size={30}' />
+                    <FaGithubSquare className='size={30}' />
+                    <FaTwitterSquare className='size={30}' />
+                </div>
+            </div>
+            <div className='lg:col-span-2 flex justify-between mt-6'>
+                <div>
+                    <h6 className='font-bold text-gray-400'>Solutions</h6>
+                    <ul>
+                        <li className='py-2 text-sm'>Analytics</li>
+                        <li className='py-2 text-sm'>Marketing</li>
+                        <li className='py-2 text-sm'>Commerce</li>
+                        <li className='py-2 text-sm'>Insights</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className='font-bold text-gray-400'>Support</h6>
+                    <ul>
+                        <li className='py-2 text-sm'>Pricing</li>
+                        <li className='py-2 text-sm'>Documentation</li>
+                        <li className='py-2 text-sm'>Guides</li>
+                        <li className='py-2 text-sm'>API Status</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className='font-bold text-gray-400'>Legal</h6>
+                    <ul>
+                        <li className='py-2 text-sm'>Claim</li>
+                        <li className='py-2 text-sm'>Policy</li>
+                        <li className='py-2 text-sm'>Terms</li>
+                        <li className='py-2 text-sm'>Condition</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className='font-bold text-gray-400'>Solutions</h6>
+                    <ul>
+                        <li className='py-2 text-sm'>Analytics</li>
+                        <li className='py-2 text-sm'>Marketing</li>
+                        <li className='py-2 text-sm'>Commerce</li>
+                        <li className='py-2 text-sm'>Insights</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/Components/Newsletter.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Newsletter = () => {
+    return (
+        <div className='w-full text-white py-16 px-4 flex flex-row '>
+            <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
+                <div className='lg:col-span-2'>
+                    <h1 className='md:text-4xl sm:text-3xl tect-xl font-bold py-2'>Eant tips & tricks to optimize your flow?</h1>
+                    <p>Sign up to our newsletter and stay up to data.</p>
+                </div>
+                <div className='my-4'>
+                    <div className='flex flex-col  sm:flex-row item-center justify-between w-full'>
+                        <input className='px-3 flex w-full rounded-md text-black h-10 mb-4' type="email" placeholder='Enter Email' />
+                        <button className='bg-[#00df9a] text-black w-[200px] rounded-md font-medium mb-4 md:ml-4 px-6 sm:tex md:mx-auto py-2'>Notify Me</button>
+                    </div>
+                    <p className=''>We care about protection of your data. Read our <span className='text-[#00df9a]'>Privacy policy</span>.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Newsletter
